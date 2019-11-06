edaae2474b5ec460cc3dccb44c1846fa4a009820
Gabe <gabe9462@gmail.com>
2019-11-06 10:46:53 -0500

    Key Bindings Implemented

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,29 +2,109 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, Runnable {
 
+    Timer tm = new Timer(1, this);
     private int width, height;
     private ArrayList<LightBike> bikeList = new ArrayList<>();
 
     public GamePanel(int width, int height){
         super();
+        this.requestFocus();
         this.setVisible(true);
         this.width = width;
         this.height = height;
-        bikeList.add(new LightBike(100, 50, Color.red));
-        bikeList.add(new LightBike(150, 100, Color.blue));
 
+        bikeList.add(new LightBike(100, 50, Color.red, 0));
+        bikeList.add(new LightBike(150, 100, Color.blue, 1));
+
+        new Thread(this).start();
+
+    }
+
+    public void run(){
+
+        InputMap im = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false), "Face Up One");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false), "Face Down One");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), "Face Right One");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false), "Face Left One");
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, false), "Face Up Two");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, false), "Face Down Two");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, false), "Face Right Two");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, false), "Face Left Two");
+
+        ActionMap ap = this.getActionMap();
+        ap.put("Face Up One", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(0).turn(LightBike.DIRECTION_UP);
+            }
+        });
+        ap.put("Face Down One", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(0).turn(LightBike.DIRECTION_DOWN);
+            }
+        });
+        ap.put("Face Right One", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(0).turn(LightBike.DIRECTION_RIGHT);
+            }
+        });
+        ap.put("Face Left One", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(0).turn(LightBike.DIRECTION_LEFT);
+            }
+        });
+        ap.put("Face Up Two", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(1).turn(LightBike.DIRECTION_UP);
+            }
+        });
+        ap.put("Face Down Two", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(1).turn(LightBike.DIRECTION_DOWN);
+            }
+        });
+        ap.put("Face Right Two", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(1).turn(LightBike.DIRECTION_RIGHT);
+            }
+        });
+        ap.put("Face Left Two", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bikeList.get(1).turn(LightBike.DIRECTION_LEFT);
+            }
+        });
+
+        tm.start();
+    }
+
+    public void actionPerformed(ActionEvent a){
+
+        for(int i = 0; i < bikeList.size(); i++){
+            bikeList.get(i).accelerate();
+        }
+
+        repaint();
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         setBackground(Color.black);
-
-        System.out.println("paintcomponent");
 
         Color stripe = new Color(0, 165, 161);
         g.setColor(stripe);
@@ -37,7 +117,6 @@ public class GamePanel extends JPanel {
         for(int i = 0; i < bikeList.size(); i++){
             LightBike currentBike = bikeList.get(i);
             g.setColor(currentBike.getColor());
-            System.out.println(currentBike.getPosX());
             g.fillRect(currentBike.getPosX(), currentBike.getPosY(), currentBike.width, currentBike.height);
         }
 
--- src/LightBike.java
+++ src/LightBike.java
@@ -1,21 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 public class LightBike extends Rectangle {
 
     private ArrayList<Rectangle> trails = new ArrayList<>();
-    private int velocity = 1, xPos, yPos;
+    private int velocity = 5, xPos, yPos;
 
     public static final int DIRECTION_UP = 0, DIRECTION_DOWN = 1, DIRECTION_LEFT = 2, DIRECTION_RIGHT = 3;
+    private int currentDirection, playerNumber;
     Color color;
 
-    public LightBike(int spawn_x, int spawn_y, Color color){
+    public LightBike(int spawn_x, int spawn_y, Color color, int playerNumber){
         super(spawn_x, spawn_y, 50, 50);
         xPos = spawn_x;
         yPos = spawn_y;
         this.color = color;
+        this.playerNumber = playerNumber;
+
+        this.currentDirection = DIRECTION_RIGHT;
     }
 
     Color getColor(){
@@ -44,6 +49,27 @@ public class LightBike extends Rectangle {
 
     void setY(int newY){
         yPos = newY;
+    }
+
+    void turn(int newDirection){
+        currentDirection = newDirection;
+    }
+
+    void accelerate(){
+        switch (currentDirection){
+            case DIRECTION_UP:
+                yPos -= velocity;
+                break;
+            case DIRECTION_DOWN:
+                yPos += velocity;
+                break;
+            case DIRECTION_LEFT:
+                xPos -= velocity;
+                break;
+            case DIRECTION_RIGHT:
+                xPos += velocity;
+                break;
+        }
     }
 
 
